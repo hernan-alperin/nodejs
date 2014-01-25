f8c764c0889d95211e89af7abf1416c8d3f49177
Hernán Alperin <hernan.alperin@gmail.com>
2014-01-24 22:29:26 -0300

    adding a file

---
+++ http.js
@@ -0,0 +1,9 @@
+var http = require('http');
+for (key in http.STATUS_CODES) {console.log('STATUS_CODES['+key+']=='+http.STATUS_CODES[key]);}
+// create http server
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('okay');
+});
+server.listen(8000)
+
